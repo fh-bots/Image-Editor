9f15ac3d82ed2cb315109ba05046fd67423db4ae
fh_bots <69167693+fh-bots@users.noreply.github.com>
2021-01-25 21:46:32 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ async def start(client: Client, message: Message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "❤️FILIMHOUSE",
+                            url="https://t.me/joinchat/Vsdqpx_0wNvfeiDx",
                         )
                     ],
                 ]
@@ -42,12 +42,8 @@ async def help(client, message):
                         InlineKeyboardButton("BACK", callback_data="start_data"),
                         InlineKeyboardButton("ABOUT", callback_data="about_data"),
                     ],
-                    [
-                        InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
-                        )
-                    ],
+       
+
                 ]
             ),
             reply_to_message_id=message.message_id,
@@ -68,12 +64,8 @@ async def about(client, message):
                         InlineKeyboardButton("BACK", callback_data="help_data"),
                         InlineKeyboardButton("START", callback_data="start_data"),
                     ],
-                    [
-                        InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
-                        )
-                    ],
+          
+                        
                 ]
             ),
             reply_to_message_id=message.message_id,
