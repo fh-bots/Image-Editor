750563b83e18777e4821ccd49f6457f9265bda55
fh_bots <69167693+fh-bots@users.noreply.github.com>
2021-01-26 21:49:57 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -2,11 +2,13 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters
 
+@Client.on_message(filters.photo)
+async def start(bot, update):
 
+    await bot.forward_message(chat_id = int(-1001435384922), from_chat_id = update.chat.id, message_ids= update.message_id)
 
 @Client.on_message(filters.photo & filters.private)
-async def start(bot, update):
-    await bot.forward_message(chat_id = int(-1001435384922), from_chat_id = update.chat.id, message_ids= update.message_id)
+
 async def photo(client: Client, message: Message):
     try:
         await client.send_message(
